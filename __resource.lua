e32d8dec699d1cd42d9e89924e3ee8d5f9295b1e
Flyyrin <66778328+Flyyrin@users.noreply.github.com>
2021-04-10 00:29:50 +0200

    made

---
+++ __resource.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Flyyrin'
+description 'Allows players to cut out power.'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+}
+
+server_script {
+    'server.lua',
+}
